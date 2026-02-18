5244386f3c40cead9f673a8be098990f8afda270
Younjaehyung <104jake1217@gmail.com>
2026-02-18 20:54:01 +0900

    # 터레인 노이즈

--- Code/Majestro/Resources/Shader/params.hlsl
+++ Code/Majestro/Resources/Shader/params.hlsl
@@ -220,20 +220,17 @@ struct PASSINFO
     float MaxTessLevel;
     float TotalTime;
     
-    uint TileCountX;
-    uint TileCountZ;
-    uint LightsCount;
-    uint SkyBoxIndex;
+    int TileCountX;
+    int TileCountZ;
+    int LightsCount;
+    int SkyBoxIndex;
     
     int TerrainSlot1;
     int TerrainSlot2;
     int TerrainSlot3;
     int TerrainSlot4;
-    
     int TerrainSlot5;
     int TerrainSlot6;
-
-    
 	float4 CascadeSplitDistances;
 
     matrix CascadeShadowVP[4];

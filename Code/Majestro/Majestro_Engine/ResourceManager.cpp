2005363f8be87326828aeaf03ff6d558d1ba1dfa
DongJoo <looloo8910@gmail.com>
2026-03-20 13:43:53 +0900

    #리듬변경

--- Code/Majestro/Majestro_Engine/ResourceManager.cpp
+++ Code/Majestro/Majestro_Engine/ResourceManager.cpp
@@ -1634,7 +1634,7 @@ void ResourceManager::CreateDefaultMaterial()
 	}
 	
 	{
-		auto Tex = Load<Texture>(L"GradientTex", L"..\\Resources\\Texture\\GradientTex.png");
+		//auto Tex = Load<Texture>(L"GradientTex", L"..\\Resources\\Texture\\GradientTex.png");
 		auto Noise = Load<Texture>(L"NoiseTex", L"..\\Resources\\Texture\\T_TilingNoise02_M.png");
 	}
 

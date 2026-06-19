a4f702c30854b995fe7b2645946dfedef8a36437
Younjaehyung <104jake1217@gmail.com>
2026-06-20 06:21:23 +0900

    # 플레이어별 스폰 위치

--- Code/Server/GameServer/PlayerSpawnComponent.h
+++ Code/Server/GameServer/PlayerSpawnComponent.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "Component.h"
+#include <array>
 
 // 플레이어 스폰 지점 마커.
 class PlayerSpawnComponent : public Component<PlayerSpawnComponent>
 {
 public:
 	Vec3 mPosition{};
+	std::array<Vec3, 3> mCharacterPositions{};
 };
